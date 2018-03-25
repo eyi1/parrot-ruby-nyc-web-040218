c3950d40a8bc8bca82f035356302fb6c8a957510
eyi1 <ej7729@gmail.com>
2018-03-25 01:05:21 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,16 +1,8 @@
-def parrot
-  puts "Squawk!"
-
-end
-
-def parrot
-puts "Squawk!"
-"Squawk!"
-end
-
-def parrot (bird = "Pretty Bird")
+def parrot (bird = "Squawk!")
   puts bird
+
 end
+
 
 
 #method `parrot` that outputs a given phrase and
